5849e4ad277fc9b1c3f9acb81fd579d34c97034d
Matheus Pedroni <pnmatheus@protonmail.com>
2024-11-10 00:25:43 -0300

    add profile to config

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import type { Site, SocialObjects } from "./types";
 export const SITE: Site = {
   website: "https://mathpn.com/",
   author: "Matheus Pedroni",
+  profile: "",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "MPN",
   ogImage: "astropaper-og.jpg",
