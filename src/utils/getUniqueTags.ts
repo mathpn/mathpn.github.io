6c9fd595260ebe56576ca011a22e7040c1730efb
Matheus Pedroni <pnmatheus@protonmail.com>
2024-12-12 13:41:48 -0300

    include TILs in tags page

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -1,20 +1,20 @@
-import { slugifyStr } from "./slugify";
-import type { CollectionEntry } from "astro:content";
+import type { PostCollectionEntry } from "types";
 import postFilter from "./postFilter";
+import { slugifyStr } from "./slugify";
 
 interface Tag {
   tag: string;
   tagName: string;
 }
 
-const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
+const getUniqueTags = (posts: PostCollectionEntry[]) => {
   const tags: Tag[] = posts
     .filter(postFilter)
-    .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
+    .flatMap((post) => post.data.tags)
+    .map((tag) => ({ tag: slugifyStr(tag), tagName: tag }))
     .filter(
       (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
+        self.findIndex((tag) => tag.tag === value.tag) === index
     )
     .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
   return tags;
