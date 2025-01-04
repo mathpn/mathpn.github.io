5da4a39719ffb344e7ccdadbabc1f4645a4d1ab2
Matheus Pedroni <pnmatheus@protonmail.com>
2025-01-04 16:04:19 -0300

    finish frameslider component

--- src/assets/js/climbing-trees/frame-slider.tsx
+++ src/assets/js/climbing-trees/frame-slider.tsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
 
-const images = import.meta.glob("../../images/climbing-trees-1/frame_*.png", {
+const images = import.meta.glob<{
+  default: { src: string; width: number; height: number; format: string };
+}>("../../images/climbing-trees-1/annimated-depth/frame_*.webp", {
   eager: true,
 });
 
 const TreeDepthSlider = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
-  const [totalFrames, setTotalFrames] = useState(15); // Matches our Python output
+  const [totalFrames, _] = useState(Object.entries(images).length);
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    let interval;
+    let interval: NodeJS.Timeout;
     if (isPlaying) {
       interval = setInterval(() => {
         setCurrentFrame((prev) => (prev === totalFrames - 1 ? 0 : prev + 1));
@@ -28,7 +30,7 @@ const TreeDepthSlider = () => {
     <div
       className="not-prose"
       style={{
-        maxWidth: "800px",
+        maxWidth: "900px",
         margin: "0 auto",
       }}
     >
@@ -41,7 +43,7 @@ const TreeDepthSlider = () => {
         }}
       >
         <img
-          src={currentImage.default.src}
+          src={currentImage?.default.src}
           style={{
             width: "100%",
             height: "auto",
@@ -61,7 +63,7 @@ const TreeDepthSlider = () => {
             onClick={() => setIsPlaying(!isPlaying)}
             style={{
               padding: "8px 16px",
-              backgroundColor: "#007bff",
+              backgroundColor: "#1863b2",
               color: "white",
               border: "none",
               borderRadius: "4px",
@@ -83,7 +85,6 @@ const TreeDepthSlider = () => {
             }}
             style={{ flex: 1 }}
           />
-
           <div style={{ minWidth: "100px", color: "black" }}>
             Depth: {currentFrame + 1}
           </div>
